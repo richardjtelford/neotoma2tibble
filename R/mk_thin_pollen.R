0b93516600f03c117074b696104f422ef5f5a0ab
richardjtelford <richard.telford@bio.uib.no>
2019-07-09 09:55:17 +0200

    extractor functions

--- R/mk_thin_pollen.R
+++ R/mk_thin_pollen.R
@@ -3,16 +3,22 @@
 #make nested tibble
 #filter out unwanted stuff
 
+
 #get taxon list
 #get dataset metadata
 #calculate percent based on count sum
 
-
+#helper function to extract species counts
 #' @importFrom dplyr gather rownames_to_column filter left_join %>% as_tibble
+#' @importFrom neotoma counts
 #' 
 
 #make thin pollen using correct column of taxon.list
-mk_thin_pollen <- function(counts, taxon.list){
+mk_thin_pollen <- function(x){
+  counts <- counts(x)
+  taxon.list <- x$taxon.list
+  
+  #some datasets have duplicate taxon.names so use alias if it exists
   if(any(names(taxon.list) == "alias")){
     name_column <- "alias"
   } else {
@@ -29,4 +35,33 @@ mk_thin_pollen <- function(counts, taxon.list){
     filter(variable.element %in% c("pollen", "spores", "ascospore")) %>% 
     as_tibble()
   return(thin_pollen)
+}
+
+
+################
+#helper functions to extract sample.meta, chronologies or lab.data
+#' @importFrom dplyr gather rownames_to_column filter left_join %>% as_tibble
+
+#extract sample.meta 
+extract_sample_meta <- function(x){
+
+   out <- x$sample.meta %>% 
+     rename(sampleID = sample.id)
+
+   return(out)
+}
+
+#extract lab.data - need to deal with inconsistent column names
+extract_lab_data <- function(x){
+  
+  #   out <- x$lab.data
+  #   if(ncol(out) == 0){
+  #     return(NULL)
+  #   }
+  # 
+  # out <- out %>%
+  #   as.data.frame() %>% 
+  #   rownames_to_column(var = "sampleID") %>%
+  #   as_tibble()
+  # return(out)
 }
